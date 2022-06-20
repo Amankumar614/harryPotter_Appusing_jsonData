1e8eb49fbe80a687f467fdb1bee822e95a859fd1
Aman kumar <yoyoamankumar614@gmail.com>
2022-06-21 00:26:38 +0530

    particular is in construction

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,23 +1,26 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:harrypotter/screens/HufflepuffList.dart';
 import 'package:harrypotter/screens/RavenclawList.dart';
 import 'package:harrypotter/screens/SlytherinList.dart';
 import 'package:harrypotter/screens/gryffindorsList.dart';
 import 'package:flutter/services.dart' as rootBundle;
+import 'package:harrypotter/screens/jsonHasData.dart';
+import 'package:harrypotter/screens/particuarHouse.dart';
 import '../Models/charaModel.dart';
 
 class home extends StatelessWidget {
   const home({Key? key}) : super(key: key);
 
-    Future<List<characterDataModel>>ReadJsonData()async{
-    final jsonData=await rootBundle.rootBundle.loadString('assets/person.json');
+  Future<List<characterDataModel>> ReadJsonData() async {
+    final jsonData =
+        await rootBundle.rootBundle.loadString('assets/person.json');
     // convert the data into list
-    final list = json.decode(jsonData) as List<dynamic>; 
+    final list = json.decode(jsonData) as List<dynamic>;
     // print(list);
     return list.map((e) => characterDataModel.fromJson(e)).toList();
-
   }
 
   @override
@@ -45,151 +48,23 @@ class home extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       body: FutureBuilder(
-        future: ReadJsonData(),
-        builder: (context,data){
-          if (data.hasError) {
-            print("error $data");
-           return Center(child: Text("error ${data.error}"),);
-          }
-          else if(data.hasData){
-           return    SingleChildScrollView(
-             child: Column(
-                     children: [
-              const SizedBox(height: 10,),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.search,color: Colors.black,),
-                    labelText: 'Enter Name',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(width: 1, color: Colors.grey)
-                 ,                  borderRadius: BorderRadius.circular(15),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(width: 1, color: Colors.grey),
-                      borderRadius: BorderRadius.circular(15),
-                    )),
-              ),),
-                     SizedBox(height: 19,),
-                     Row(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.only(left:15.0),
-                  child: Text("Categories",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                ),
-              ],
-                     ),
-                     Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: 
-              
-              const [
-                Text("Gryffindors",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                
-                Text("See All",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.grey),),
-              ],),
-                 
-                     ),
-                     Container(height: 200,child: gryffindors(data:data)),
-                     Padding(
-              padding: const EdgeInsets.all(08.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: 
-                
-                const [
-                  Text("Slytherin",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                  
-                  Text("See All",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.grey),),
-                ],),
-                     ),
-                     Container(height: 200,child: Slytherin(data:data)),
-                     Padding(
-              padding: const EdgeInsets.all(08.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: 
-                
-                const [
-                  Text("Hufflepuff",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                  
-                  Text("See All",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.grey),),
-                ],),
-                     ),
-                     Container(height: 200,child: Hufflepuff(data:data)),
-                     Padding(
-                       padding: const EdgeInsets.all(8.0),
-                       child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: 
-                
-                const [
-                  Text("Ravenclaw",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                  
-                  Text("See All",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.grey),),
-                ],),
-                     ),
-                     Container(height: 200,child: Ravenclaw(data:data)),
-                     SizedBox(height: 20,)
-                     ],
-                   ),
-           );
-
-          }
-          else{
-            return Container( height:900,child: Center(child: CircularProgressIndicator(),));
-          }
-        }
-
-      //   child: Column(
-      //     children: [
-      //       SizedBox(height: 10,),
-      //       Padding(
-      //         padding: const EdgeInsets.all(8.0),
-      //         child: TextField(
-                
-      //         decoration: InputDecoration(
-      //           prefixIcon: Icon(Icons.search,color: Colors.black,),
-      //             labelText: 'Enter Name',
-      //             enabledBorder: OutlineInputBorder(
-      //               borderSide: const BorderSide(width: 1, color: Colors.grey)
-      // ,                  borderRadius: BorderRadius.circular(15),
-      //             ),
-      //             focusedBorder: OutlineInputBorder(
-      //               borderSide: const BorderSide(width: 1, color: Colors.grey),
-      //               borderRadius: BorderRadius.circular(15),
-      //             )),
-      //       ),),
-      //     SizedBox(height: 19,),
-      //     Row(
-      //       children: [
-      //         Padding(
-      //           padding: const EdgeInsets.only(left:15.0),
-      //           child: Text("Categories",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-      //         ),
-      //       ],
-      //     ),
-      //     Padding(
-      //       padding: const EdgeInsets.all(8.0),
-      //       child: Row(
-      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //         children: 
-            
-      //       [
-      //         Text("Gryffindors",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-              
-      //         Text("See All",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.grey),),
-      //       ],),
-      
-      //     ),
-      //     Container(height: 200,child: gryffindors())
-      //     ],
-      //   ),
-      ),
+          future: ReadJsonData(),
+          builder: (context, data) {
+            if (data.hasError) {
+              print("error $data");
+              return Center(
+                child: Text("error ${data.error}"),
+              );
+            } else if (data.hasData) {
+              return jsonHasData(data: data,);
+            } else {
+              return Container(
+                  height: 900,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ));
+            }
+          }),
     );
   }
 }
--- lib/screens/particuarHouse.dart
+++ lib/screens/particuarHouse.dart
@@ -1,13 +1,36 @@
+// import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class particularHouse extends StatelessWidget {
-final houseData;
-particularHouse({this.houseData});
+import '../Models/charaModel.dart';
 
+class particularHouse extends StatelessWidget {
+  final houseData;
+  final houseName;
+  particularHouse({required this.houseData, required this.houseName});
   @override
   Widget build(BuildContext context) {
+    var item = houseData.data as List<characterDataModel>;
+
     return Scaffold(
-      appBar: AppBar(centerTitle: true,title: Text("House name"),),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(houseName),
+        ),
+        body: GridView.builder(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemCount: 2,
+          itemBuilder: (context, i) {
+            print(item[i].house);
+            return item[i].house!=houseName?SizedBox():GridTile(
+                child: Container(
+                    color: Colors.amber,
+                    child: Image.network(
+                      item[i].image.toString(),
+                      fit: BoxFit.fill,
+                    )));
+          },
+        ));
   }
 }
